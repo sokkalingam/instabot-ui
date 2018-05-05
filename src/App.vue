e7ef551457eea80492361235e0709e513ba90068
Sokkalingam Subramanian <sokkalingam.subramanian@amadeus.com>
2018-05-05 07:39:29 -0400

    Better UI

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
+    <div class="logo">
+      <img class="logo" src="./assets/instabot-logo.png">
+    </div>
     <router-view/>
   </div>
 </template>
@@ -16,8 +18,23 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  text-align: center;
+  background-color: white;
+  height: 100%;
 }
+
+html {
+  height: 100%;
+}
+
+body {
+  background-image: url('./assets/instabot-bg.jpg');
+  height: 100%;
+}
+
+.logo {
+  height: 200px;
+}
+
 </style>
--- src/components/job/CreateJob.vue
+++ src/components/job/CreateJob.vue
@@ -1,40 +1,80 @@
 <template>
 
-  <form id="createJobForm">
-    <div class="form-group">
-      <label for="username">Instagram Username:</label>
-      <input type="text" v-model="formData.username" class="form-control" id="username">
+  <div id="createJob">
+
+    <div v-if='formVisible'>
+
+      <form>
+
+        <div class="row">
+          <div class="form-group form-inline col-md-12">
+            <label for="presetName">Preset Name:</label>
+            <input type="text" v-model="presetName" class="form-control" id="presetName">
+            <button type="button" v-on:click="lookupPreset" class="btn btn-info">Lookup Preset</button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="form-group col-md-6">
+            <label for="sessionId">Session Id</label>
+            <textarea type="text" v-model="formData.sessionId" class="form-control" id="sessionId" rows="5"></textarea>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="hashtags">Hashtags (comma separated)</label>
+            <textarea type="text" v-model="formData.hashtagText" class="form-control"
+              v-on:change="formatHashtag" id="hashtags" rows="5"></textarea>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="form-group col-md-4">
+            <label for="noOfPhotos">Number of Photos:</label>
+            <input type="number" v-model="formData.noOfPhotos" class="form-control" id="noOfPhotos">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="noOfTimesToLoop">Loop Count:</label>
+            <input type="number" v-model="formData.noOfTimesToLoop" class="form-control" id="noOfTimesToLoop">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="maxNoOfFollowers">Maximum Followers:</label>
+            <input type="number" v-model="formData.maxNoOfFollowers" class="form-control" id="maxNoOfFollowers">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="form-group col-md-4">
+            <label for="timeMin">Minimum Wait Time:</label>
+            <input type="number" v-model="formData.timeMin" class="form-control" id="timeMin">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="timeMax">Maximum Wait Time:</label>
+            <input type="number" v-model="formData.timeMax" class="form-control" id="timeMax">
+          </div>
+        </div>
+
+      </form>
+      <div class="row">
+        <div class="col-xs-2">
+          <button type="button" v-on:click="runJob" class="btn btn-primary btn-lg">Run Job</button>
+        </div>
+        <div class="col-xs-2">
+          <button type="button" v-on:click="savePreset" class="btn btn-warning btn-lg">Save Preset</button>
+        </div>
+      </div>
+
     </div>
-    <div class="form-group">
-      <label for="sessionId">Session Id:</label>
-      <textarea type="text" v-model="formData.sessionId" class="form-control" id="sessionId"></textarea>
+
+    <button type="button" class="btn btn-lg btn-info"
+      v-on:click="showForm" v-if="!formVisible">Submit Another Job</button>
+
+    <div class="alert alert-success" role="alert" v-if="successMessage">
+      <p>{{successMessage}}</p>
     </div>
-    <div class="form-group">
-      <label for="noOfPhotos">Number of Photos:</label>
-      <input type="number" v-model="formData.noOfPhotos" class="form-control" id="noOfPhotos">
+    <div class="alert alert-danger" role="alert" v-else-if="errorMessage">
+      <p>{{errorMessage}}</p>
     </div>
-    <div class="form-group">
-      <label for="noOfTimesToLoop">Loop Count:</label>
-      <input type="number" v-model="formData.noOfTimesToLoop" class="form-control" id="noOfTimesToLoop">
-    </div>
-    <div class="form-group">
-      <label for="timeMin">Minimum Wait Time:</label>
-      <input type="number" v-model="formData.timeMin" class="form-control" id="timeMin">
-    </div>
-    <div class="form-group">
-      <label for="timeMax">Maximum Wait Time:</label>
-      <input type="number" v-model="formData.timeMax" class="form-control" id="timeMax">
-    </div>
-    <div class="form-group">
-      <label for="maxNoOfFollowers">Maximum Followers:</label>
-      <input type="number" v-model="formData.maxNoOfFollowers" class="form-control" id="maxNoOfFollowers">
-    </div>
-    <div class="form-group">
-      <label for="hashtags">Hashtags (comma separated):</label>
-      <textarea type="text" v-model="formData.hashtagText" class="form-control" id="hashtags"></textarea>
-    </div>
-    <button type="button" v-on:click="submitForm" class="btn btn-primary btn-lg">Submit</button>
-  </form>
+
+  </div>
 
 </template>
 
@@ -45,43 +85,100 @@ export default {
   name: 'create-job',
   data() {
     return {
-      formData: {
-        username: '',
-        sessionId: '',
+      formVisible: true,
+      presetName: '',
+      successMessage: '',
+      errorMessage: '',
+      formData: this.getDefaultForm()
+    }
+  },
+  methods: {
+    getDefaultForm() {
+      return {
+        sessionId: 'PASTE YOUR SESSION ID HERE',
         noOfPhotos: 5,
         noOfTimesToLoop: 10,
         timeMin: 25,
         timeMax: 30,
         maxNoOfFollowers: 500,
-        hashtagText: '',
+        hashtagText: 'TypeYourhashtagsHere, CommaSeparated',
         hashtags: []
-      }
-    }
-  },
-  methods: {
-    processHashtags() {
-        this.formData.hashtags = this.formData.hashtagText.split(",");
+      };
     },
-    submitForm() {
-      this.processHashtags();
-      console.log(this.formData);
+    showForm () {
+      this.formVisible = true;
+    },
+    hideForm () {
+      this.formVisible = false;
+    },
+    formatHashtag() {
+      console.log(this.formData.hashtagText);
+      this.hashtagTextToArray();
+      this.hashtagArrayToText();
+    },
+    hashtagTextToArray() {
+      this.formData.hashtags = this.formData.hashtagText.replaceAll(" ", "")
+        .toLowerCase().split(",");
+    },
+    hashtagArrayToText() {
+      this.formData.hashtagText = this.formData.hashtags.join(", ");
+    },
+    runJob() {
+      this.hashtagTextToArray();
       this.$http.post(`${ConfigConstants.SERVER_BASE_URL}/hashtag/looplike`, this.formData)
         .then((response) => {
           console.log(response);
+          this.hideForm();
+          this.successMessage = response.bodyText;
+        }).catch((response) => {
+          console.log(response);
+          let errorObj = response.body.errors[0];
+          this.errorMessage = errorObj.field + ' ' + errorObj.defaultMessage;
+        });
+    },
+    lookupPreset() {
+      this.$http.get(`${ConfigConstants.SERVER_BASE_URL}/presets/${this.presetName}`)
+        .then((response) => {
+          console.log(response);
+          this.formData = response.body.data;
+          this.hashtagArrayToText();
         }).catch((error) => {
           console.log(error);
         });
+    },
+    savePreset() {
+      this.$http.post(`${ConfigConstants.SERVER_BASE_URL}/presets/add`, {
+        name: this.presetName,
+        data: this.formData
+      }).then((response) => {
+          console.log(response);
+          this.successMessage = response.bodyText;
+      }).catch((response) => {
+          console.log(response);
+          let errorObj = response.body.errors[0];
+          this.errorMessage = errorObj.field + ' - ' + errorObj.defaultMessage;
+      });
     }
   }
 }
 
 </script>
 
-<style>
+<style scoped>
 
-  form {
+  #createJob {
     text-align: left;
-    padding: 50px;
+    font-size: 18px;
+  }
+
+  .row {
+      padding-top: 10px;
+      padding-bottom: 10px;
+  }
+
+  .alert {
+      margin-top: 10px;
+      margin-bottom: 10px;
   }
 
 </style>
