ac8cd661eedb414d53d2799697bdf6da6f33e79a
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2019-01-27 15:50:41 -0500

    New Urls in Home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,18 +2,20 @@
   <div>
     <h3>{{title}}</h3>
     <div class="alert alert-success" role="alert">
-      <h4 class="blink">New Features</h4>
+      <h4 class="blink">New URLs</h4>
+      <h5>If you are not able to access instabot, try one of the following URLs</h5>
       <ul>
-        <p><b>Email Notifications</b> - Enter your email in Create Job page and save Preset</p>
-        <p><b>Feedback Page</b> - Provide feedback, report bugs, feature requests</p>
+        <p><a href="http://instabot.localtunnel.me">http://instabot.localtunnel.me</a></p>
+        <p><a href="http://instabot1.localtunnel.me">http://instabot1.localtunnel.me</a></p>
+        <p><a href="http://instabot2.localtunnel.me">http://instabot2.localtunnel.me</a></p>
       </ul>
     </div>
     <div class="alert alert-info" role="alert">
       <h4>Let instabot like photos for you, while you are busy taking new ones</h4>
     </div>
-    <div class="alert alert-warning" role="alert">
+    <!-- <div class="alert alert-warning" role="alert">
       <p>If the website is down, please try again in 30 seconds</p>
-    </div>
+    </div> -->
   </div>
 </template>
 
