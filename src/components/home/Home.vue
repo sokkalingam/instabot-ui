74e2cf2cc6da6932fbe7a4860d80ad3985d751b3
Sokkalingam Subramanian <sokkalingam.subramanian@zoominfo.com>
2019-03-10 22:49:20 -0400

    part_34_changes_for_instabot360: Ui changes for instabot360

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,21 +1,9 @@
 <template>
   <div>
     <h3>{{title}}</h3>
-    <div class="alert alert-success" role="alert">
-      <h4 class="blink">New URLs</h4>
-      <h5>If you are not able to access instabot, try one of the following URLs</h5>
-      <ul>
-        <p><a href="http://instabot.localtunnel.me">http://instabot.localtunnel.me</a></p>
-        <p><a href="http://instabot1.localtunnel.me">http://instabot1.localtunnel.me</a></p>
-        <p><a href="http://instabot2.localtunnel.me">http://instabot2.localtunnel.me</a></p>
-      </ul>
-    </div>
     <div class="alert alert-info" role="alert">
       <h4>Let instabot like photos for you, while you are busy taking new ones</h4>
     </div>
-    <!-- <div class="alert alert-warning" role="alert">
-      <p>If the website is down, please try again in 30 seconds</p>
-    </div> -->
   </div>
 </template>
 
